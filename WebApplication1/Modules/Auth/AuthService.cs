32a35a4bf0c44a865b0484b583b9e902b3b3dff0
Георгий Кобунов <ancient.gekob@gmail.com>
2023-09-21 20:46:40 +0300

    some refactoring

--- WebApplication1/Modules/Auth/AuthService.cs
+++ WebApplication1/Modules/Auth/AuthService.cs
@@ -6,8 +6,10 @@ public interface IAuthService
 
 public class AuthService : IAuthService
 {
+    private readonly int _hashWorkFactor = 12;
+
     public void Register()
     {
-        string hashedPassword = BCrypt.Net.BCrypt.HashPassword("password", 12);
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword("password", _hashWorkFactor);
     }
 }
